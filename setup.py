cf16cff173c07ff775bea9b831011358a1e87da3
DamenTheDev <damenthedev@gmail.com>
2022-02-12 01:53:38 +1030

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gino',
+    version='1.0.0.0',
+    packages=['tests', 'mysql_tests'],
+    url='https://github.com/DamenTheDev/gino.git',
+    license='',
+    author='Sam',
+    author_email='dontatme@gmail.com',
+    description='The thing'
+)
